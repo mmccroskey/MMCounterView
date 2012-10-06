f082ae0e4beb9b93855ac7f2952538616f257f11
Matthew McCroskey <matthew.mccroskey@gmail.com>
2012-10-05 22:00:19 -0500

    Updated Demo's ViewController and Storyboard to show multiple counters of different shapes and sizes.

--- Demo/Demo/Classes/Controllers/ViewController/ViewController.m
+++ Demo/Demo/Classes/Controllers/ViewController/ViewController.m
@@ -9,50 +9,73 @@
 #import "ViewController.h"
 #import "MMCounterView.h"
 
+#define counterView1Tag 1
+#define counterView2Tag 2
+#define counterView3Tag 3
+#define counterView4Tag 4
+
 @interface ViewController ()
 
-@property (weak) IBOutlet MMCounterView *counterView;
-@property NSInteger previousInteger;
+@property (weak) IBOutlet MMCounterView *counterView1;
+@property (weak) IBOutlet MMCounterView *counterView2;
+@property (weak) IBOutlet MMCounterView *counterView3;
+@property (weak) IBOutlet MMCounterView *counterView4;
+
+@property (strong) NSMutableDictionary *previousIntegers;
+
+- (void)updateValueForCounterView:(MMCounterView*)counterView;
+- (NSInteger)previousIntegerForTag:(NSInteger)tag;
+- (void)setPreviousInteger:(NSInteger)integer forTag:(NSInteger)tag;
 
 @end
 
 @implementation ViewController
 
-@synthesize counterView = counterView_;
-@synthesize previousInteger = previousInteger_;
+@synthesize counterView1 = counterView1_;
+@synthesize counterView2 = counterView2_;
+@synthesize counterView3 = counterView3_;
+@synthesize counterView4 = counterView4_;
+
+@synthesize previousIntegers = previousIntegers_;
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
     
-    self.previousInteger = 0;
-    [self performSelector:@selector(updateValue) withObject:nil afterDelay:1.5];
-}
-
-- (void)updateValue
-{
-    self.previousInteger = self.counterView.integer;
-    NSInteger integer = (arc4random() % 120);
-    [self.counterView setInteger:integer];
+    self.previousIntegers = [[NSMutableDictionary alloc] initWithCapacity:4];
     
-    NSInteger numberOfTicks = abs(integer-self.previousInteger);
-    [self performSelector:@selector(updateValue) withObject:nil afterDelay:((1.0/self.counterView.ticksPerSecond)*numberOfTicks+1)];
+    [self.counterView1 setTag:counterView1Tag];
+    [self.counterView2 setTag:counterView2Tag];
+    [self.counterView3 setTag:counterView3Tag];
+    [self.counterView4 setTag:counterView4Tag];
+    
+    [self performSelector:@selector(updateValueForCounterView:) withObject:self.counterView1 afterDelay:1.5];
+    [self performSelector:@selector(updateValueForCounterView:) withObject:self.counterView2 afterDelay:1.5];
+    [self performSelector:@selector(updateValueForCounterView:) withObject:self.counterView3 afterDelay:1.5];
+    [self performSelector:@selector(updateValueForCounterView:) withObject:self.counterView4 afterDelay:1.5];
 }
 
-
-- (void)viewDidUnload
+- (void)updateValueForCounterView:(MMCounterView*)counterView
 {
-    [super viewDidUnload];
-    // Release any retained subviews of the main view.
+    NSInteger previousInteger = counterView.integer;
+    NSInteger newInteger = (arc4random() % 120);
+    NSInteger numberOfTicks = abs(newInteger-previousInteger);
+    
+    [self setPreviousInteger:previousInteger forTag:counterView.tag];
+    [counterView setInteger:newInteger];
+    
+    // Update this counterView again one second after this update is complete
+    [self performSelector:@selector(updateValueForCounterView:) withObject:counterView afterDelay:((1.0/counterView.ticksPerSecond)*numberOfTicks+1)];
 }
 
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+- (NSInteger)previousIntegerForTag:(NSInteger)tag
 {
-    if ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPhone) {
-        return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
-    } else {
-        return YES;
-    }
+    return [(NSNumber*)[self.previousIntegers objectForKey:[NSNumber numberWithInteger:tag]] integerValue];
+}
+
+- (void)setPreviousInteger:(NSInteger)integer forTag:(NSInteger)tag
+{
+    [self.previousIntegers setObject:[NSNumber numberWithInteger:integer] forKey:[NSNumber numberWithInteger:tag]];
 }
 
 @end
